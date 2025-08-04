05aeeec42a4311359401faa839735775cacc1f13
Gabriel-Lippi-Costa <gabriellippidacosta@gmail.com>
2025-08-04 10:12:13 -0300

    Tela pronta de elementos está pronta

--- CodigoDoProjeto/src/main/java/com/mycompany/codigodoprojeto/telas/EscolherGerenciamento.java
+++ CodigoDoProjeto/src/main/java/com/mycompany/codigodoprojeto/telas/EscolherGerenciamento.java
@@ -7,7 +7,7 @@ public class EscolherGerenciamento extends javax.swing.JFrame {
   public EscolherGerenciamento() {
     super("Gerenciamento Acadêmico");
     initComponents();
-    setLocationRelativeTo(null);
+    setLocationRelativeTo(null);    
   }
 
   @SuppressWarnings("unchecked")
@@ -15,21 +15,21 @@ public class EscolherGerenciamento extends javax.swing.JFrame {
   private void initComponents() {
 
     jPanel1 = new javax.swing.JPanel();
-    jLabel1 = new javax.swing.JLabel();
+    jLabel2 = new javax.swing.JLabel();
     jButton1 = new javax.swing.JButton();
     jButton2 = new javax.swing.JButton();
     jButton3 = new javax.swing.JButton();
 
     setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-    getContentPane().setLayout(new java.awt.GridLayout());
+    setBackground(new java.awt.Color(255, 255, 255));
 
     jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
-    jLabel1.setFont(new java.awt.Font("Arial", 1, 34)); // NOI18N
-    jLabel1.setText("O que você deseja fazer?");
+    jLabel2.setFont(new java.awt.Font("Arial", 1, 34)); // NOI18N
+    jLabel2.setText("O que você deseja fazer?");
 
-    jButton1.setBackground(new java.awt.Color(255, 102, 102));
-    jButton1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+    jButton1.setBackground(new java.awt.Color(255, 0, 0));
+    jButton1.setFont(new java.awt.Font("Arial", 1, 22)); // NOI18N
     jButton1.setForeground(new java.awt.Color(255, 255, 255));
     jButton1.setText("VER ALUNOS");
     jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -38,13 +38,13 @@ public class EscolherGerenciamento extends javax.swing.JFrame {
       }
     });
 
-    jButton2.setBackground(new java.awt.Color(255, 102, 102));
-    jButton2.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+    jButton2.setBackground(new java.awt.Color(255, 0, 0));
+    jButton2.setFont(new java.awt.Font("Arial", 1, 22)); // NOI18N
     jButton2.setForeground(new java.awt.Color(255, 255, 255));
     jButton2.setText("VER PROFESSORES");
 
-    jButton3.setBackground(new java.awt.Color(255, 102, 102));
-    jButton3.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+    jButton3.setBackground(new java.awt.Color(255, 0, 0));
+    jButton3.setFont(new java.awt.Font("Arial", 1, 22)); // NOI18N
     jButton3.setForeground(new java.awt.Color(255, 255, 255));
     jButton3.setText("VER CURSOS");
 
@@ -52,40 +52,49 @@ public class EscolherGerenciamento extends javax.swing.JFrame {
     jPanel1.setLayout(jPanel1Layout);
     jPanel1Layout.setHorizontalGroup(
       jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-        .addContainerGap(201, Short.MAX_VALUE)
-        .addComponent(jLabel1)
-        .addGap(189, 189, 189))
       .addGroup(jPanel1Layout.createSequentialGroup()
-        .addGap(290, 290, 290)
-        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-          .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE)
-          .addComponent(jButton2)
-          .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 212, javax.swing.GroupLayout.PREFERRED_SIZE))
-        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+          .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGap(199, 199, 199)
+            .addComponent(jLabel2))
+          .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGap(271, 271, 271)
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+              .addComponent(jButton2)
+              .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 252, javax.swing.GroupLayout.PREFERRED_SIZE)
+              .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 252, javax.swing.GroupLayout.PREFERRED_SIZE))))
+        .addContainerGap(206, Short.MAX_VALUE))
     );
     jPanel1Layout.setVerticalGroup(
       jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
       .addGroup(jPanel1Layout.createSequentialGroup()
-        .addGap(16, 16, 16)
-        .addComponent(jLabel1)
-        .addGap(142, 142, 142)
-        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addGap(22, 22, 22)
+        .addComponent(jLabel2)
+        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 136, Short.MAX_VALUE)
+        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
         .addGap(18, 18, 18)
-        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
         .addGap(18, 18, 18)
-        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
-        .addContainerGap(87, Short.MAX_VALUE))
+        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addGap(219, 219, 219))
     );
 
-    getContentPane().add(jPanel1);
+    javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+    getContentPane().setLayout(layout);
+    layout.setHorizontalGroup(
+      layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+    );
+    layout.setVerticalGroup(
+      layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+      .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+    );
 
     pack();
   }// </editor-fold>//GEN-END:initComponents
 
   private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-    new GerenciamentoAlunos().setVisible(true);
-    this.dispose();
+    // TODO add your handling code here:
   }//GEN-LAST:event_jButton1ActionPerformed
 
   public static void main(String args[]) {
@@ -97,7 +106,7 @@ public class EscolherGerenciamento extends javax.swing.JFrame {
   private javax.swing.JButton jButton1;
   private javax.swing.JButton jButton2;
   private javax.swing.JButton jButton3;
-  private javax.swing.JLabel jLabel1;
+  private javax.swing.JLabel jLabel2;
   private javax.swing.JPanel jPanel1;
   // End of variables declaration//GEN-END:variables
 }
