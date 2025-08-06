f75619af1e9a9189662269041e3f2d2d843f48ee
Gabriel-Lippi-Costa <gabriellippidacosta@gmail.com>
2025-08-06 09:00:13 -0300

    Classe Professor está pronta

---
+++ CodigoDoProjeto/src/main/java/com/mycompany/codigodoprojeto/modelos/Professor.java
@@ -0,0 +1,51 @@
+package com.mycompany.codigodoprojeto.modelos;
+
+public class Professor {
+  
+  private String nome;
+  private String ensina;
+  private String email;
+  private String senha;
+  private String cpf;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getEnsina() {
+    return ensina;
+  }
+
+  public void setEnsina(String ensina) {
+    this.ensina = ensina;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getSenha() {
+    return senha;
+  }
+
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+  
+}
